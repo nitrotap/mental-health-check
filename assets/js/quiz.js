d0bdf1d38a5e6df8c2fb175fb4b825f295ce4028
nitrotap <kartikinpublic@gmail.com>
2022-02-02 02:17:58 -0700

    adding text area question logic

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,26 +5,18 @@
 // variable declarations
 let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-
-// set up content variables
-let textAreaEl = document.createElement("textarea")
-
-
 // building questions
 // response types
 let responseType = { // an object with distinct types
     yesNo: ["Yes", "No"],
     weekday: weekdays,
     frequency: ["Not at all", "Not frequently", "A couple times a month", "A couple times a week", "Every Day"],
-    userInput: textAreaEl,
+    userInput: [],
 }
-
-
 /*  Questions in this format
     {question: "",
     response: responseType.frequency},
 */
-
 let depressionQuestions = [ // an array of objects depressionQuestions[0].question, depressionQuestions[1].response
     {question: "How often are you having thoughts that you would be better off dead, or of hurting yourself?",
         response: responseType.frequency},
@@ -56,9 +48,9 @@ let otherQuestions = [ // an array of objects
     ];
 
 let impairmentQuestions = [ // an array of objects
-      {question: "What is the Weekday?", response: responseType.userInput},
+      {question: "What is the Weekday?", response: responseType.weekday},
       {question: "Type in the first three things you see", response: responseType.userInput},
-      {questionName: "What is the year?", response: responseType.userInput}
+      {question: "What is the year?", response: responseType.userInput}
     ];
 
 let anxietyQuestions = [  // an array of objects
@@ -78,10 +70,7 @@ let anxietyQuestions = [  // an array of objects
     response: responseType.frequency},
 ];
 
-let titleDivEl = document.createElement("div")
-let titleTextEl = document.createElement("h1")
-
-function dQuestion(questionObj) { // takes {question, response}
+function radioQuestion(questionObj) { // takes {question, response}
     let contentDivEl = document.querySelector("#content");
     let questionTextDivEl = document.createElement("div")
     let questionTextEl = document.createElement("h3")
@@ -107,24 +96,50 @@ function dQuestion(questionObj) { // takes {question, response}
     contentDivEl.appendChild(questionResponseDivEl)
 
 
-} // displays single question and answers
+} // displays single radio question and answers
+
+function textAreaQuestion(questionObj) {
+    let contentDivEl = document.querySelector("#content");
+    let questionTextDivEl = document.createElement("div")
+    let questionTextEl = document.createElement("h3")
+    questionTextEl.textContent = questionObj.question;
+    questionTextDivEl.appendChild(questionTextEl)
+    contentDivEl.appendChild(questionTextDivEl)
+
+    let questionResponseDivEl = document.createElement("div")
+    let questionResponseTextAreaEl = document.createElement("textarea")
+    questionResponseDivEl.appendChild(questionResponseTextAreaEl)
+    contentDivEl.appendChild(questionResponseDivEl)
+} // displays single textarea question and answers
+
+function questionParser(questionObj) {
+    if(questionObj.response === responseType.userInput) {
+        textAreaQuestion(questionObj)
+    } else {
+        radioQuestion(questionObj)
+    }
+
+} // takes a question object and parses its type
+
+function displayAll() {
+    for (let i = 0; i < impairmentQuestions.length; i++) {
+        questionParser(impairmentQuestions[i])
+    }
+    for (let i = 0; i < otherQuestions.length; i++) {
+        questionParser(otherQuestions[i])
+    }
+    for (let i = 0; i < depressionQuestions.length; i++) {
+        questionParser(depressionQuestions[i])
+    }
+    for (let i = 0; i < anxietyQuestions.length; i++) {
+        questionParser(anxietyQuestions[i])
+    }
+
+} // TEST FUNCTION displays all questions TODO remove
 
 
 function main() {
-
-    for (let i = 0; i < otherQuestions.length; i++) {
-        dQuestion(otherQuestions[i])
-    }
-
-
-    /*
-    for (let i = 0; i < depressionQuestions.length; i++) {
-        dQuestion(depressionQuestions[i])
-    }*/
-    /*
-    for (let i = 0; i < anxietyQuestions.length; i++) {
-        dQuestion(anxietyQuestions[i])
-    }*/
+    displayAll()
 }
 
 main();
