459ef573a31aee69aa600cf7f6355856c5a06d02
nitrotap <kartikinpublic@gmail.com>
2022-02-04 16:46:00 -0700

    adding ptsd and addiction

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,14 +4,19 @@
 
 // variable declarations
 let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let seasons = ["Spring", "Summer", "Winter", "Fall"]
+let seasons = ["Spring", "Summer", "Winter", "Fall"];
 
 // building questions
 // response types
 let responseType = { // an object with distinct types
-    yesNo: ["Yes", "No"],
+    yesNo: [{text: "Yes", score: 1}, {text: "No", score: 0}],
     weekday: weekdays,
-    frequency: ["Not at all", "Not frequently", "A couple times a month", "A couple times a week", "Every Day"],
+    frequency: [
+        {text: "Not at all", score: 0},
+        {text: "Not frequently", score: 1},
+        {text: "A couple times a month", score: 2},
+        {text: "A couple times a week", score: 3},
+        {text: "Every Day", score: 4}],
     userInput: [],
     season: seasons
 }
@@ -19,37 +24,43 @@ let responseType = { // an object with distinct types
     {question: "",
     response: responseType.frequency},
 */
-let depressionQuestions = [ // an array of objects depressionQuestions[0].question, depressionQuestions[1].response
-    {question: "How often are you having thoughts that you would be better off dead, or of hurting yourself?",
-        response: responseType.frequency},
-    {question: "How often are you moving or speaking so slowly that other people have noticed",
-        response: responseType.frequency},
-    {question: "How often are you having trouble concentrating on things such as reading the newspaper or watching TV?",
-        response: responseType.frequency},
-    {question: "How often are you feeling bad about yourself (feel like a failure or let your family down)",
-        response: responseType.frequency},
-    {question: "How often do you have a poor appetite or overeating?",
-        response: responseType.frequency},
-    {question: "How often are you feeling tired or having little energy?",
-        response: responseType.frequency},
-    {question: "How often are you having trouble falling or staying asleep, or sleeping too much?",
-        response: responseType.frequency},
-    {question: "How often are you feeling down, depressed, or hopeless?",
-        response: responseType.frequency},
-    {question: "Do you have little interest or please in doing things?",
-        response: responseType.frequency},
-    {question: "How difficult have these problems made it for you to work, at home, or with others?",
-        response: responseType.frequency}
+let depressionQuestions = [ // an array of objects depressionQuestions[0].question, depressionQuestions[1].response PHQ-9
+    {question: "Are you having thoughts that you would be better off dead, or of hurting yourself?",
+        response: responseType.yesNo},
+    {question: "Are you moving or speaking so slowly that other people have noticed",
+        response: responseType.yesNo},
+    {question: "Are you having trouble concentrating on things such as reading the newspaper or watching TV?",
+        response: responseType.yesNo},
+    {question: "Are you feeling bad about yourself (feel like a failure or let your family down)",
+        response: responseType.yesNo},
+    {question: "Do you have a poor appetite or are you overeating?",
+        response: responseType.yesNo},
+    {question: "Are you feeling tired or having little energy?",
+        response: responseType.yesNo},
+    {question: "Are you having trouble falling or staying asleep, or sleeping too much?",
+        response: responseType.yesNo},
+    {question: "Are you feeling down, depressed, or hopeless?",
+        response: responseType.yesNo},
+    {question: "Do you have little interest or pleasure in doing things?",
+        response: responseType.yesNo},
 ]
 
-let otherQuestions = [ // an array of objects
+let ptsdQuestions = [
+    {question: "Are you having nightmares about a distressing event(s) or thought about the event(s) when you did not want to?", response: responseType.yesNo},
+    {question: "Are you trying hard not to think about a distressing event(s) or went out of your way to avoid situations that reminded you of the event(s)?", response: responseType.yesNo},
+    {question: "Do you feel constantly on guard, watchful, or easily startled?", response: responseType.yesNo},
+    {question: "Do you feel numb or detached from people, activities, or your surroundings?", response: responseType.yesNo},
+    {question: "Do you feel guilty or unable to stop blaming yourself or others for a distressing events(s) or any problems the event(s) may have caused?", response: responseType.yesNo},
+]
+
+let schizophreniaQuestions = [ // an array of objects
     {question: "Are the happy thoughts speeding up your thought process?", response: responseType.yesNo},
     {question: "Are the sad thoughts slowing down your thought process?", response: responseType.yesNo},
     {question: "How much brain fog are you experiencing?", response: responseType.frequency},
     {question: "Have you had any grandiose thoughts?", response: responseType.frequency}
 ];
 
-let impairmentQuestions = [ // an array of objects
+let impairmentQuestions = [ // an array of objects - these questions have right or wrong answers
     {question: "What is the Weekday?", response: responseType.weekday},
     {question: "Type in the first three things you see", response: responseType.userInput},
     {question: "What is the year?", response: responseType.userInput},
@@ -57,22 +68,40 @@ let impairmentQuestions = [ // an array of objects
     {question: "Spell 'WORLD' backwards", response: responseType.userInput},
 ];
 
-let anxietyQuestions = [  // an array of objects
-    {question: "How often have you been feeling nervous, anxious, or on edge",
-        response: responseType.frequency},
-    {question: "How often have you been not able to stop or control worrying",
-        response: responseType.frequency},
-    {question: "How often have you been worrying too much about different things",
-        response: responseType.frequency},
-    {question: "How often have you been having trouble relaxing",
-        response: responseType.frequency},
-    {question: "How often have you been so restless that it is hard to sit still",
-        response: responseType.frequency},
-    {question: "How often have you been easily annoyed or irritable",
-        response: responseType.frequency},
-    {question: "How often have you been feeling as if something awful might happen?",
-        response: responseType.frequency},
+let addictionQuestions = [
+    {question: "Are you using substances to numb any physical or emotional pain?", response: responseType.yesNo},
+    {question: "Do you feel like you should cut down on your substance use?", response: responseType.yesNo},
+    {question: "Are you feeling guilty about using substances?", response: responseType.yesNo},
+    {question: "Is anyone annoying you by criticizing your substance use?", response: responseType.yesNo},
+    {question: "Do you feel that your substance use significantly decreases your ability to function?", response: responseType.yesNo},
+    {question: "Are you using substances as soon as you wake up in the morning?", response: responseType.yesNo}
+]
+
+let anxietyQuestions = [  // an array of objects GAD-7
+    {question: "Are you feeling nervous, anxious, or on edge?",
+        response: responseType.yesNo},
+    {question: "Are you feeling unable to stop or control worrying?",
+        response: responseType.yesNo},
+    {question: "Are you worrying too much about different things?",
+        response: responseType.yesNo},
+    {question: "Are you having trouble relaxing?",
+        response: responseType.yesNo},
+    {question: "Are you so restless that it is hard to sit still?",
+        response: responseType.yesNo},
+    {question: "Are you feeling easily annoyed or irritable?",
+        response: responseType.yesNo},
+    {question: "Are you feeling as if something awful might happen?",
+        response: responseType.yesNo},
 ];
+
+
+
+
+
+
+
+
+
 
 function radioQuestion(questionObj) { // takes {question, response}
     let contentDivEl = document.querySelector("#content");
@@ -93,7 +122,7 @@ function radioQuestion(questionObj) { // takes {question, response}
 
         let questionResponseLabelEl = document.createElement("label")
         questionResponseLabelEl.setAttribute("for", questionObj.response[i])
-        questionResponseLabelEl.textContent = questionObj.response[i]
+        questionResponseLabelEl.textContent = questionObj.response[i].text
 
         questionResponseDivEl.appendChild(questionResponseInputEl)
         questionResponseDivEl.appendChild(questionResponseLabelEl)
@@ -130,8 +159,8 @@ function displayAll() {
     for (let i = 0; i < impairmentQuestions.length; i++) {
         questionParser(impairmentQuestions[i])
     }
-    for (let i = 0; i < otherQuestions.length; i++) {
-        questionParser(otherQuestions[i])
+    for (let i = 0; i < schizophreniaQuestions.length; i++) {
+        questionParser(schizophreniaQuestions[i])
     }
     for (let i = 0; i < depressionQuestions.length; i++) {
         questionParser(depressionQuestions[i])
@@ -139,6 +168,13 @@ function displayAll() {
     for (let i = 0; i < anxietyQuestions.length; i++) {
         questionParser(anxietyQuestions[i])
     }
+    for (let i = 0; i < ptsdQuestions.length; i++) {
+        questionParser(ptsdQuestions[i])
+    }
+    for (let i = 0; i < addictionQuestions.length; i++) {
+        questionParser(addictionQuestions[i])
+    }
+
 
 } // TEST FUNCTION displays all questions TODO remove
 
@@ -162,7 +198,6 @@ function questionDisplay(questionObj) {
 }
 
 questionOptions = [];
-
 function questionChooserButtonHandler (userQuestions) {
     let contentDivEl = document.querySelector("#content");
 
@@ -229,14 +264,29 @@ function quizSubmitButtonHandler() {
                 console.log(quizResults)
             }
         }
+
+        // Depression 10 questions
+        // if 1-4 minimal depression
+        // if 5-9 mild depression
+        // if 10-14 moderate depression
+        // if 15-19 moderately severe depression
+        // if 20-27 severe depression
+
+        // Anxiety 7 questions
+
+
+
+
     });
 }
 
-function questionChooser() { // user picks and stores relevant questions
+function questionChooser() {
     let contentDivEl = document.querySelector("#content");
     questionDisplay(impairmentQuestions)
     questionDisplay(depressionQuestions)
-    questionDisplay(otherQuestions)
+    questionDisplay(schizophreniaQuestions)
+    questionDisplay(ptsdQuestions)
+    questionDisplay(addictionQuestions)
 
     // add checked object to array
     let questionChooserSubmitButton = document.createElement("button")
@@ -254,7 +304,7 @@ function questionChooser() { // user picks and stores relevant questions
         questionChooserButtonHandler(userQuestions);
     })
     contentDivEl.appendChild(questionChooserSubmitButton)
-}
+} // user picks and stores relevant questions
 
 function main() {
     // displayAll()
