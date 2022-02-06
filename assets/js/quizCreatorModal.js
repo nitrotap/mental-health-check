006737068a90fa3cfd425d35f9a4a8f2ed2dd421
MesH <meshtatsuo@gmail.com>
2022-02-06 11:29:53 -0700

    added comments to quizCreatorModal

--- assets/js/quizCreatorModal.js
+++ assets/js/quizCreatorModal.js
@@ -1,3 +1,6 @@
+// Quiz creator modal management
+// Manages the launching
+
 var quizCreationModal = $(".modal");
 
 // Launch modal whem page finished loading
@@ -12,11 +15,21 @@ $(document).ready(function () {
 quizCreationModal.on("click", ".quiz-begin", function () {
   //Call quiz start function here
   console.log("Begin Quiz");
+  // Gather info from checkboxes required for quiz creation
+  // here
+
   quizCreationModal.modal("close");
+
+  // Generate and begin quiz AFTER closing modal!
 });
 
 quizCreationModal.on("click", ".cancel", function () {
   //Call quiz start function here
   console.log("Cancelled, going to home");
   quizCreationModal.modal("close");
+
+  // Will redirect back to homepage after closing.
+  // disabled for now.
+
+  //window.location.replace("index.html");
 });
