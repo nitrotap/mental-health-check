a6fdc426418de3b6f0cd85afd2f753677d7a6cc9
MesH <meshtatsuo@gmail.com>
2022-02-05 18:50:12 -0700

    basic work on landing page

---
+++ assets/js/quizCreatorModal.js
@@ -0,0 +1,10 @@
+// Code to run quiz creation modal
+
+$(".ui.modal").modal("show");
+
+$(".slider").glide({
+  autoplay: false,
+  arrowsWrapperClass: "slider-arrows",
+  arrowRightText: "",
+  arrowLeftText: "",
+});
