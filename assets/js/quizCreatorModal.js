b9640df83d95d990bea68636833c01fd57c3ed8d
MesH <meshtatsuo@gmail.com>
2022-02-06 11:26:13 -0700

    modal functional. Just needs to call quiz start function

--- assets/js/quizCreatorModal.js
+++ assets/js/quizCreatorModal.js
@@ -1,5 +1,22 @@
+var quizCreationModal = $(".modal");
+
 // Launch modal whem page finished loading
 $(document).ready(function () {
-  console.log("launching modal");
+  // modal must be initialized before opening
   $(".modal").modal();
+  // launch modal
+  $(".modal").modal("open");
+});
+
+// listener for modal buttons
+quizCreationModal.on("click", ".quiz-begin", function () {
+  //Call quiz start function here
+  console.log("Begin Quiz");
+  quizCreationModal.modal("close");
+});
+
+quizCreationModal.on("click", ".cancel", function () {
+  //Call quiz start function here
+  console.log("Cancelled, going to home");
+  quizCreationModal.modal("close");
 });
