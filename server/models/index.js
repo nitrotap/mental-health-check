2b4bf52408cf8c4e9bfa0178fd50ccf342e47693
nitrotap <kartikinpublic@gmail.com>
2022-05-22 19:46:49 -0600

    adding recording to models index

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const QuizSet = require('./QuizSet');
 const QuizResult = require('./QuizResult');
+const Recording = require('./Recording')
 
-
-module.exports = { User, QuizSet, QuizResult };
+module.exports = { User, QuizSet, QuizResult, Recording };
