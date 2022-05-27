b28db09edfbe9b6ce9d536ba54fe6b44d822090f
Cha Vue <chavue09@gmail.com>
2022-05-26 21:07:21 -0500

    login and signup styled. Logic isn't working yet.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,75 +5,188 @@ depends on: LOGIN mutation
 assigned to:
 */
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
-function Login(props) {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error }] = useMutation(LOGIN);
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const mutationResponse = await login({
-                variables: { email: formState.email, password: formState.password },
-            });
-            console.log(mutationResponse)
-            const token = mutationResponse.data.login.token;
-            Auth.login(token);
-        } catch (e) {
-            console.log(e);
-        }
-    };
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
-
+function Copyright(props) {
     return (
-        <div className="container my-1">
-            <Link to="/signup">← Go to Signup</Link>
-
-            <h2>Login</h2>
-            <form onSubmit={handleFormSubmit}>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="email">Email address:</label>
-                    <input
-                        placeholder="youremail@test.com"
-                        name="email"
-                        type="email"
-                        id="email"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="pwd">Password:</label>
-                    <input
-                        placeholder="******"
-                        name="password"
-                        type="password"
-                        id="pwd"
-                        onChange={handleChange}
-                    />
-                </div>
-                {error ? (
-                    <div>
-                        <p className="error-text">The provided credentials are incorrect</p>
-                    </div>
-                ) : null}
-                <div className="flex-row flex-end">
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="/homepage">
+          Mental Health Check
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
     );
+  }
+
+  const theme = createTheme();
+
+export default function Login() {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleFormSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default Login;
+
+// function Login(props) {
+//     const [formState, setFormState] = useState({ email: '', password: '' });
+//     const [login, { error }] = useMutation(LOGIN);
+
+//     const handleFormSubmit = async (event) => {
+//         event.preventDefault();
+//         try {
+//             const mutationResponse = await login({
+//                 variables: { email: formState.email, password: formState.password },
+//             });
+//             console.log(mutationResponse)
+//             const token = mutationResponse.data.login.token;
+//             Auth.login(token);
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     };
+
+//     const handleChange = (event) => {
+//         const { name, value } = event.target;
+//         setFormState({
+//             ...formState,
+//             [name]: value,
+//         });
+//     };
+
+//     return (
+//         <div className="container my-1">
+//             <Link to="/signup">← Go to Signup</Link>
+
+//             <h2>Login</h2>
+//             <form onSubmit={handleFormSubmit}>
+//                 <div className="flex-row space-between my-2">
+//                     <label htmlFor="email">Email address:</label>
+//                     <input
+//                         placeholder="youremail@test.com"
+//                         name="email"
+//                         type="email"
+//                         id="email"
+//                         onChange={handleChange}
+//                     />
+//                 </div>
+//                 <div className="flex-row space-between my-2">
+//                     <label htmlFor="pwd">Password:</label>
+//                     <input
+//                         placeholder="******"
+//                         name="password"
+//                         type="password"
+//                         id="pwd"
+//                         onChange={handleChange}
+//                     />
+//                 </div>
+//                 {error ? (
+//                     <div>
+//                         <p className="error-text">The provided credentials are incorrect</p>
+//                     </div>
+//                 ) : null}
+//                 <div className="flex-row flex-end">
+//                     <button type="submit">Submit</button>
+//                 </div>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -9,68 +9,200 @@ assigned to:
 */
 
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 
-function Signup(props) {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [addUser] = useMutation(ADD_USER);
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        const mutationResponse = await addUser({
-            variables: {
-                email: formState.email,
-                password: formState.password,
-            },
-        });
-        const token = mutationResponse.data.addUser.token;
-        Auth.login(token);
-    };
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
-
-    return (
-        <div className="container my-1">
-            <Link to="/login">← Go to Login</Link>
-
-            <h2>Signup</h2>
-            <form onSubmit={handleFormSubmit}>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        placeholder="youremail@test.com"
-                        name="email"
-                        type="email"
-                        id="email"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="pwd">Password:</label>
-                    <input
-                        placeholder="******"
-                        name="password"
-                        type="password"
-                        id="pwd"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row flex-end">
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
-    );
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="/homepage">
+        Mental Health Check
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Signup;
+const theme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+// function Signup(props) {
+//     const [formState, setFormState] = useState({ email: '', password: '' });
+//     const [addUser] = useMutation(ADD_USER);
+
+//     const handleFormSubmit = async (event) => {
+//         event.preventDefault();
+//         const mutationResponse = await addUser({
+//             variables: {
+//                 email: formState.email,
+//                 password: formState.password,
+//             },
+//         });
+//         const token = mutationResponse.data.addUser.token;
+//         Auth.login(token);
+//     };
+
+//     const handleChange = (event) => {
+//         const { name, value } = event.target;
+//         setFormState({
+//             ...formState,
+//             [name]: value,
+//         });
+//     };
+
+//     return (
+//         <div className="container my-1">
+//             <Link to="/login">← Go to Login</Link>
+
+//             <h2>Signup</h2>
+//             <form onSubmit={handleFormSubmit}>
+//                 <div className="flex-row space-between my-2">
+//                     <label htmlFor="email">Email:</label>
+//                     <input
+//                         placeholder="youremail@test.com"
+//                         name="email"
+//                         type="email"
+//                         id="email"
+//                         onChange={handleChange}
+//                     />
+//                 </div>
+//                 <div className="flex-row space-between my-2">
+//                     <label htmlFor="pwd">Password:</label>
+//                     <input
+//                         placeholder="******"
+//                         name="password"
+//                         type="password"
+//                         id="pwd"
+//                         onChange={handleChange}
+//                     />
+//                 </div>
+//                 <div className="flex-row flex-end">
+//                     <button type="submit">Submit</button>
+//                 </div>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default Signup;
