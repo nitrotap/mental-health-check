4da90071497f905ee3b5166dbd4a263526d582df
Will Yazdani <yazd0006@gmail.com>
2022-05-28 12:38:35 -0500

    removed redundant icons imports

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import HomeIcon from '@mui/icons-material/Home';
 import DashboardIcon from '@mui/icons-material/Dashboard';
