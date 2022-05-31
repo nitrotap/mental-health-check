f28b0bfcf1a962c91fea71028d0fb41874148ff0
nitrotap <9121718+nitrotap@users.noreply.github.com>
2022-05-30 20:53:29 -0600

    re-adding wildcard

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
