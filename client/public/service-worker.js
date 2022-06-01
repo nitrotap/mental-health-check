7f4a0e91b1d8fade4bb4aa45c0296577e023e0d7
Colby Harris <harris.colby@gmail.com>
2022-06-01 02:29:13 -0500

    added some cache functionality, not really working

---
+++ client/public/service-worker.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-restricted-globals */
+const APP_PREFIX = 'mhc-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard',
+  '/quizselect',
+  '/legal',
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  let cacheWhitelist = ['mhc_pwa'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { serviceWorkerRegistration } from 'react';
 import ReactDOM from 'react-dom/client';
+
 
 import './components/CSS/index.css';
 
@@ -20,3 +21,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
